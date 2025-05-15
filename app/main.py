af14cd0937728fe3cb9de78d5575ea7c2a282bec
Alex Araujo <alexaraujo@iMac-de-Alex.local>
2025-05-15 16:46:26 -0400

    .

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import os
+import random
+import string
+from pathlib import Path
 from langchain_ollama import OllamaLLM
+
+OUTPUT_DIR = Path("respostas")
+OUTPUT_DIR.mkdir(exist_ok=True)
 
 OLLAMA_API = os.getenv("OLLAMA_API", "http://ollama:11434")
 llm = OllamaLLM(model="gemma3:1b", base_url=OLLAMA_API)
@@ -10,8 +16,22 @@ app = FastAPI()
 
 class Pergunta(BaseModel):
     prompt: str
+    
+    
+def gerar_nome_arquivo():
+    letras = string.ascii_lowercase + string.digits
+    nome = ''.join(random.choices(letras, k=10))
+    return OUTPUT_DIR / f"{nome}.txt"
 
 @app.post("/perguntar")
 def perguntar(pergunta: Pergunta):
     resposta = llm.invoke(pergunta.prompt)
-    return {"resposta": resposta}
+    
+    caminho_arquivo = gerar_nome_arquivo()
+    
+    with open(caminho_arquivo, "w", encoding="utf-8") as f:
+        f.write(resposta)
+
+    return {"resposta": resposta, "arquivo": str(caminho_arquivo)}
+    
+    # return {"resposta": resposta}
